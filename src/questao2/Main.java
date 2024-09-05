cec587a3e03ca1e26e8cb02274322b005776f5f4
marcelojacint <marcelojacintoferreirq@gmail.com>
2024-09-05 00:39:10 -0300

    questão 2 concluida

--- src/questao2/Main.java
+++ src/questao2/Main.java
@@ -1,8 +1,47 @@
 package questao2;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+
+        if (VerificaSequenciaFibonacci(numero)) {
+            System.out.println(numero + " está na sequência de Fibonacci.");
+        } else {
+            System.out.println(numero + " não está na sequência de Fibonacci.");
+        }
+
+        scanner.close();
+
+    }
+
+    public static boolean VerificaSequenciaFibonacci(int num) {
+
+        if (num < 0) {
+            return false;
+        }
+
+        if (num == 0 || num == 1) {
+            return true;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        while (b < num) {
+            int auxiliar = a + b;
+            a = b;
+            b = auxiliar;
+        }
+
+        return b == num;
     }
 }
+
